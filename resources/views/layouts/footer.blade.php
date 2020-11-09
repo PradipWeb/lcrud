ecfc495b76abd868a6522a699e4aea06c83296a4
Pradip Harichandra Khatik <73273668+PradipWeb@users.noreply.github.com>
2020-11-09 11:01:49 +0530

    Add Files Via Upload

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+ 
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Pradip Harichandra Khatik
+    <a href=" "> easylearningbd.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
